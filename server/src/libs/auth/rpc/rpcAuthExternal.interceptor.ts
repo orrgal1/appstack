8b2c4845d53a61b79b619bdb82b31c4a165047dd
orgal <or.gal@vee.com>
2023-10-27 16:49:51 +0300

    rate limiting done

--- server/src/libs/auth/rpc/rpcAuthExternal.interceptor.ts
+++ server/src/libs/auth/rpc/rpcAuthExternal.interceptor.ts
@@ -5,7 +5,6 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 
-// TODO: apply user based read/write rate limiting on external calls
 @Injectable()
 export class RpcAuthExternalInterceptor implements NestInterceptor {
   async intercept(context: ExecutionContext, next: CallHandler): Promise<any> {

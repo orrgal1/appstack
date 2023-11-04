5b6802c8fe10475ab654df33b1ed0d62c70124ea
orgal <or.gal@vee.com>
2023-11-04 21:31:07 +0200

    json-to-grpc gateway+generated client+integration to web-client. great success!

--- service1/src/services/infra/health/health.controller.ts
+++ service1/src/services/infra/health/health.controller.ts
@@ -5,6 +5,7 @@ import { HealthChekcResult } from '../../../proto/interfaces';
 @Controller()
 export class HealthController {
   @GrpcMethod('HealthService', 'HealthCheck')
+  //TODO: typo in HealthChekcResult
   async healthCheck(): Promise<HealthChekcResult> {
     return { ok: true };
   }

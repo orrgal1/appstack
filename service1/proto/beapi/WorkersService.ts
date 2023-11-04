3962ab120b650e08ee7d4e00364d3cb00f1319b7
orgal <or.gal@vee.com>
2023-11-04 21:29:19 +0200

    json-to-grpc gateway+generated client+integration to web-client. great success!

---
+++ service1/proto/beapi/WorkersService.ts
@@ -0,0 +1,34 @@
+import { Empty } from './google/protobuf/empty';
+export interface PublishJobInput {
+  dummyJobPayload: DummyJobPayload | undefined;
+}
+
+export interface DummyJobPayload {
+  id: string;
+}
+
+export interface PublishJobResult {
+  jobId: string;
+}
+
+export interface WorkersHealthCheckResult {
+  ok: boolean;
+}
+
+import { postToUnary } from './utils';
+
+export class WorkersService {
+  private readonly serviceName: string = 'WorkersService';
+
+  async publishJob(data: PublishJobInput): Promise<PublishJobResult> {
+    return postToUnary<PublishJobResult>(this.serviceName, 'publishJob', data);
+  }
+
+  async healthCheck(data: Empty): Promise<WorkersHealthCheckResult> {
+    return postToUnary<WorkersHealthCheckResult>(
+      this.serviceName,
+      'healthCheck',
+      data,
+    );
+  }
+}

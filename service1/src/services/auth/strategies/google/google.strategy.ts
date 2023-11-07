b6f518a452ac431daaa457fe042f48534d5ecb3f
orgal <or.gal@vee.com>
2023-11-07 20:27:46 +0200

    tried to debug the google auth 502 bug

--- service1/src/services/auth/strategies/google/google.strategy.ts
+++ service1/src/services/auth/strategies/google/google.strategy.ts
@@ -7,7 +7,7 @@ import { StrategyService } from '../strategy.service';
 
 config();
 
-// TODO: first login after service comes up always fails. maybe something is lazy init.
+// TODO: sometimes the auth fails with 502 and then the browser gets stuck on a 502 message. might be a strictly local problem but not sure.
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(private strategyService: StrategyService) {
